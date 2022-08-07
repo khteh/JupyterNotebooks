dcc8b80adfa4808716073b4c103a5f4edaaed86b
Kok How, Teh <funcoolgeek@gmail.com>
2022-08-07 09:53:11 +0800

    V1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jupiter Notebook using IPython kernel
+
+## Kernel
+
+- Use `/usr/bin/python` kernel selection from the top-right corner.
---
+++ imports.py
@@ -0,0 +1,6 @@
+%matplotlib inline
+import sys
+!{sys.executable} -m pip install --user numpy pandas matplotlib seaborn
+import numpy, pandas, matplotlib, seaborn
+import matplotlib.pyplot as pyplot
+seaborn.set(style="darkgrid")
