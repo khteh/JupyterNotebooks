5837dba848160c6d3fa13de04739b0697107ff75
Kok How, Teh <funcoolgeek@gmail.com>
2025-09-19 17:02:10 +0800

    Add deep NN forward and backward calculations

--- Coursera/LogisticRegression/test_utils.py
+++ Coursera/LogisticRegression/test_utils.py
@@ -1,116 +1,104 @@
 import numpy as np
-from copy import deepcopy
 
-
-def datatype_check(expected_output, target_output, error, level=0):
+def datatype_check(expected_output, target_output, error):
     success = 0
-    if (level == 0):
-        try:
-            assert isinstance(target_output, type(expected_output))
-            return 1
-        except:
-            return 0
-    else:
-        if isinstance(expected_output, tuple) or isinstance(expected_output, list) \
-                or isinstance(expected_output, np.ndarray) or isinstance(expected_output, dict):
-            if isinstance(expected_output, dict):
-                range_values = expected_output.keys()
-            else:
-                range_values = range(len(expected_output))
-            if len(expected_output) != len(target_output) or not isinstance(target_output, type(expected_output)):
-                return 0
-            for i in range_values:
-                try:
-                    success += datatype_check(expected_output[i],
-                                            target_output[i], error, level - 1)
-                except:
-                    print("Error: {} in variable {}, expected type: {}  but expected type {}".format(error,
-                                                                                                    i,
-                                                                                                    type(
-                                                                                                        target_output[i]),
-                                                                                                    type(expected_output[i]
-                                                                                                        )))
-            if success == len(expected_output):
-                return 1
-            else:
-                return 0
-
-        else:
+    if isinstance(target_output, dict):
+        for key in expected_output.keys():
             try:
-                assert isinstance(target_output, type(expected_output))
-                return 1
+                success += datatype_check(expected_output[key], 
+                                         target_output[key], error)
             except:
-                return 0
-
-
+                print("Error: {} in variable {}. Got {} but expected type {}".format(error,
+                                                                          key, type(target_output[key]), type(expected_output[key])))
+        if success == len(target_output.keys()):
+            return 1
+        else:
+            return 0
+    elif isinstance(target_output, tuple) or isinstance(target_output, list):
+        for i in range(len(expected_output)):
+            try: 
+                success += datatype_check(expected_output[i], 
+                                         target_output[i], error)
+            except:
+                print("Error: {} in variable {}. Got type: {}  but expected type {}".format(error,
+                                                                          i, type(target_output[i]), type(expected_output[i])))
+        if success == len(target_output):
+            return 1
+        else:
+            return 0
+                
+    else:
+        assert isinstance(target_output, type(expected_output))
+        return 1
+            
 def equation_output_check(expected_output, target_output, error):
     success = 0
-    if isinstance(expected_output, tuple) or isinstance(expected_output, list) or isinstance(expected_output, dict):
-        if isinstance(expected_output, dict):
-            range_values = expected_output.keys()
-        else:
-            range_values = range(len(expected_output))
-
-        if len(expected_output) != len(target_output):
-                return 0
-
-        for i in range_values:
+    if isinstance(expected_output, dict):
+        for key in expected_output.keys():
             try:
-                success += equation_output_check(expected_output[i],
-                                                 target_output[i], error)
+                success += equation_output_check(expected_output[key], 
+                                         target_output[key], error)
             except:
-                print("Error: {} for variable in position {}.".format(error, i))
-        if success == len(expected_output):
+                print("Error: {} for variable {}.".format(error,
+                                                                          key))
+        if success == len(target_output.keys()):
             return 1
         else:
             return 0
-
-    else:
-        try:
-            if hasattr(expected_output, 'shape'):
-                np.testing.assert_array_almost_equal(
-                    target_output, expected_output)
-            else:
-                assert target_output == expected_output
-        except:
+    elif isinstance(expected_output, tuple) or isinstance(expected_output, list):
+        for i in range(len(expected_output)):
+            try: 
+                success += equation_output_check(expected_output[i], 
+                                         target_output[i], error)
+            except:
+                print("Error: {} for variable {}.".format(error, i))
+        if success == len(target_output):
+            return 1
+        else:
             return 0
+                
+    else:
+        if hasattr(expected_output, 'shape'):
+            #np.allclose(target_output, expected_output)
+            np.testing.assert_array_almost_equal(target_output, expected_output)
+        else:
+            assert target_output == expected_output
         return 1
-
-
+    
 def shape_check(expected_output, target_output, error):
     success = 0
-    if isinstance(expected_output, tuple) or isinstance(expected_output, list) or \
-            isinstance(expected_output, dict) or isinstance(expected_output, np.ndarray):
-        if isinstance(expected_output, dict):
-            range_values = expected_output.keys()
-        else:
-            range_values = range(len(expected_output))
-
-        if len(expected_output) != len(target_output):
-                return 0
-        for i in range_values:
+    if isinstance(expected_output, dict):
+        for key in expected_output.keys():
             try:
-                success += shape_check(expected_output[i],
-                                       target_output[i], error)
+                success += shape_check(expected_output[key], 
+                                         target_output[key], error)
+            except:
+                print("Error: {} for variable {}.".format(error, key))
+        if success == len(expected_output.keys()):
+            return 1
+        else:
+            return 0
+    elif isinstance(expected_output, tuple) or isinstance(expected_output, list):
+        for i in range(len(expected_output)):
+            try: 
+                success += shape_check(expected_output[i], 
+                                         target_output[i], error)
             except:
                 print("Error: {} for variable {}.".format(error, i))
         if success == len(expected_output):
             return 1
         else:
             return 0
-
+                
     else:
+        if hasattr(expected_output, 'shape'):
+            assert target_output.shape == expected_output.shape
         return 1
-
-
+                
 def single_test(test_cases, target):
     success = 0
     for test_case in test_cases:
         try:
-            if 'do_before' in test_case:
-                if callable(test_case['do_before']):
-                    test_case['do_before']()
-                
             if test_case['name'] == "datatype_check":
                 assert isinstance(target(*test_case['input']),
                                   type(test_case["expected"]))
@@ -120,54 +108,36 @@ def single_test(test_cases, target):
                                    target(*test_case['input']))
                 success += 1
             if test_case['name'] == "shape_check":
-                assert test_case['expected'].shape == target(
-                    *test_case['input']).shape
+                assert test_case['expected'].shape == target(*test_case['input']).shape
                 success += 1
         except:
             print("Error: " + test_case['error'])
-
+            
     if success == len(test_cases):
         print("\033[92m All tests passed.")
     else:
-        print('\033[92m', success, " Tests passed")
+        print('\033[92m', success," Tests passed")
         print('\033[91m', len(test_cases) - success, " Tests failed")
-        raise AssertionError(
-            "Not all tests were passed for {}. Check your equations and avoid using global variables inside the function.".format(target.__name__))
-
-
+        raise AssertionError("Not all tests were passed for {}. Check your equations and avoid using global variables inside the function.".format(target.__name__))
+        
 def multiple_test(test_cases, target):
     success = 0
     for test_case in test_cases:
         try:
-            if 'do_before' in test_case:
-                print("Here")
-                if callable(test_case['do_before']):
-                    test_case['do_before']()
-                    print("Executed")
-            test_input = deepcopy(test_case['input'])
-            target_answer = target(*test_input)
-        except:
-            print('\33[30m', "Error, interpreter failed when running test case with these inputs: " + 
-                  str(test_input))
-            raise AssertionError("Unable to successfully run test case.".format(target.__name__))
-
-        try:
+            target_answer = target(*test_case['input'])                   
             if test_case['name'] == "datatype_check":
-                success += datatype_check(test_case['expected'],
-                                      target_answer, test_case['error'])
+                success += datatype_check(test_case['expected'], target_answer, test_case['error'])
             if test_case['name'] == "equation_output_check":
-                success += equation_output_check(
-                    test_case['expected'], target_answer, test_case['error'])
+                success += equation_output_check(test_case['expected'], target_answer, test_case['error'])
             if test_case['name'] == "shape_check":
-                success += shape_check(test_case['expected'],
-                                   target_answer, test_case['error'])
+                success += shape_check(test_case['expected'], target_answer, test_case['error'])
         except:
-            print('\33[30m', "Error: " + test_case['error'])
-
+            print("Error: " + test_case['error'])
+            
     if success == len(test_cases):
         print("\033[92m All tests passed.")
     else:
-        print('\033[92m', success, " Tests passed")
+        print('\033[92m', success," Tests passed")
         print('\033[91m', len(test_cases) - success, " Tests failed")
-        raise AssertionError(
-            "Not all tests were passed for {}. Check your equations and avoid using global variables inside the function.".format(target.__name__))
+        raise AssertionError("Not all tests were passed for {}. Check your equations and avoid using global variables inside the function.".format(target.__name__))
+        
