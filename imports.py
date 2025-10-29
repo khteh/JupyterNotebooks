8eca11916a1b5c4e567b4e932db62ebdbe5265f7
Kok How, Teh <funcoolgeek@gmail.com>
2025-10-29 15:40:15 +0800

    Remove unused .ipynb

--- imports.py
+++
@@ -1,7 +0,0 @@
-# Be sure to include this magic before you import Matplotlib, as it may not work if you do not
-%matplotlib inline
-import sys
-!{sys.executable} -m pip install --user numpy pandas matplotlib seaborn
-import numpy, pandas, matplotlib, seaborn
-import matplotlib.pyplot as pyplot
-seaborn.set(style="darkgrid")
